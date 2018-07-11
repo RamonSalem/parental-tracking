5c3c94e957b31cc8bbbc38d007323712eb3bfd4e
Ramon Salém <salemramon@gmail.com>
2018-07-10 21:13:58 -0300

    Map configured

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { GoogleMapsComponent } from './google-maps/google-maps';
+import { IonicModule } from 'ionic-angular';
+
 @NgModule({
 	declarations: [GoogleMapsComponent],
-	imports: [],
+	imports: [IonicModule],
 	exports: [GoogleMapsComponent]
 })
 export class ComponentsModule {}
--- src/components/google-maps/google-maps.ts
+++ src/components/google-maps/google-maps.ts
@@ -1,22 +1,57 @@
-import { Component } from '@angular/core';
-
-/**
- * Generated class for the GoogleMapsComponent component.
- *
- * See https://angular.io/api/core/Component for more info on Angular
- * Components.
- */
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NavController } from 'ionic-angular';
+ 
+declare var google;
+ 
 @Component({
   selector: 'google-maps',
   templateUrl: 'google-maps.html'
 })
 export class GoogleMapsComponent {
+ 
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
 
-  text: string;
-
-  constructor() {
-    console.log('Hello GoogleMapsComponent Component');
-    this.text = 'Hello World';
+  markers:any[] = [];
+ 
+  constructor(public navCtrl: NavController) {
+  }
+ 
+  ngOnInit(){
+    this.loadMap();
+  }
+ 
+  loadMap(){
+    let latLng = new google.maps.LatLng(-7.2064099, -35.9206093);
+ 
+    let mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+ 
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+ 
   }
 
+  addMarker(){
+    console.log("HERE");
+    let here = {lat:-7.2064099, lng: -35.9206093};
+    let marker = new google.maps.Marker({position : here, map: this.map});
+    this.markers.push(marker);
+   /* let infoWindow = new google.maps.InfoWindow({
+      content: '<h4>something</h4>'
+    });
+    
+    google.maps.event.addListener(marker, 'click', () => {
+      infoWindow.open(this.map, marker);
+    });*/
+  }
+
+  deleteMarker(){
+    if(this.markers.length>0){
+      this.markers[this.markers.length-1].setMap(null);
+      this.markers.pop();
+    }
+  }
 }
